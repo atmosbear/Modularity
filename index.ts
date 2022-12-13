4c3693fbfe7031684aabba431fd125aaf94d6d32
atmosbear <mondegreen84@protonmail.com>
2022-12-13 09:50:52 -0800

    Added generic module class!

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ function el(id: string): HTMLElement {
     return document.getElementById(id)!
 }
 
-
 let catimg = "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=8"
 
 type XY = { x: number, y: number }
@@ -11,26 +10,49 @@ function styleModule(position: XY, element: HTMLElement) {
     Object.assign(element.style, { top: position.y + "px", left: position.x + "px", position: "absolute" })
 }
 
-class IMGModule {
+class GenericModule {
     constructor(
         public position: XY,
-        public imgsrc: string,
-        public element: HTMLImageElement = document.createElement("img")
+        public element: HTMLElement,
+        public onscreen: boolean = false,
+        public childrenModules: GenericModule[] = []
     ) {
         styleModule(position, element)
-        this.element.src = imgsrc
+        modules.push(this)
     }
 
-    move(newPos: XY) {
+    moveTo(newPos: XY) {
         this.position.y = newPos.x
         this.position.x = newPos.y
         this.element.style.top = this.position.y + "px"
         this.element.style.left = this.position.x + "px"
     }
+
+    show() {
+        document.body.append(this.element);
+        this.onscreen = true
+    }
+
+    hide() {
+        document.body.removeChild(this.element)
+        this.onscreen = false
+    }
 }
 
-function addModuleToScreen(what: any) { document.body.append(what.element); console.log(what.element) }
-let a = new IMGModule({ x: 100, y: 300 }, catimg)
-addModuleToScreen(a)
-a.move({x: 30, y: 20})
-a.move({x: -300, y: -100})
+class ImageModule extends GenericModule  {
+    constructor(
+        public position: XY,
+        public imgsrc: string,
+        public element: HTMLImageElement = document.createElement("img")
+    ) {
+        super(position, element)
+        this.element.src = imgsrc
+    }
+}
+
+let modules: GenericModule[] = []
+let a = new ImageModule({ x: 100, y: 300 }, catimg)
+a.show()
+a.moveTo({ x: 30, y: 20 })
+a.moveTo({ x: -300, y: -100 })
+a.element.onclick = () => {a.moveTo({x: 3, y: 200})}
