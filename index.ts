f1fd0f6998ccf7f32925c648684d4bc8fe16f1c8
atmosbear <mondegreen84@protonmail.com>
2022-12-13 09:24:03 -0800

    Changed class implementation to independence

--- index.ts
+++ index.ts
@@ -2,28 +2,35 @@ function el(id: string): HTMLElement {
     return document.getElementById(id)!
 }
 
-type XY = {
-    x: number,
-    y: number
+
+let catimg = "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=8"
+
+type XY = { x: number, y: number }
+
+function styleModule(position: XY, element: HTMLElement) {
+    Object.assign(element.style, { top: position.y + "px", left: position.x + "px", position: "absolute" })
 }
 
-class Module {
-    constructor (
-        public position: XY 
-    ) {}
-}
+class IMGModule {
+    constructor(
+        public position: XY,
+        public imgsrc: string,
+        public element: HTMLImageElement = document.createElement("img")
+    ) {
+        styleModule(position, element)
+        this.element.src = imgsrc
+    }
 
-class TextModule extends Module {
-    constructor(position: XY) {
-        super(position)
+    move(newPos: XY) {
+        this.position.y = newPos.x
+        this.position.x = newPos.y
+        this.element.style.top = this.position.y + "px"
+        this.element.style.left = this.position.x + "px"
     }
 }
 
-class ImageModule extends Module {
-    constructor(position: XY, public imagesrc: string) {
-        super(position)
-    }
-}
-
-let img = new ImageModule({x: 10, y: 10}, "hi")
-console.log(img.imagesrc)
+function addModuleToScreen(what: any) { document.body.append(what.element); console.log(what.element) }
+let a = new IMGModule({ x: 100, y: 300 }, catimg)
+addModuleToScreen(a)
+a.move({x: 30, y: 20})
+a.move({x: -300, y: -100})
